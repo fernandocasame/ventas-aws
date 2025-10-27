cad585ae58bd0aa63418fe5dc1b2280dacd40d62
fcasame <fcasame@amapola.tech>
2025-10-27 12:15:25 -0500

    invoice

--- src/app/walmart/page.tsx
+++ src/app/walmart/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef } from 'react';
 import jsPDF from 'jspdf';
 import html2canvas from 'html2canvas';
 
